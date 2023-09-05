e9ab382841f788d7e2686fd9f398a21bf3c25784
nick <nick2019gnn@gmail.com>
2023-09-05 13:44:51 +0300

    BDBOT-2: add getFileName

--- src/main/java/my/notify/bd/jsonUtil/JsonUtil.java
+++ src/main/java/my/notify/bd/jsonUtil/JsonUtil.java
@@ -5,15 +5,23 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class JsonUtil {
+
     private static final Logger logger = Logger.getLogger(JsonUtil.class.getName());
 
     public static void createJson(String chatId) {
-        File json = new File("D://" + chatId + ".json");
+        File json = new File(getFileName(chatId));
 
         try {
             boolean newFile = json.createNewFile();
+
+            logger.info("Created file is: " + newFile);
         } catch (IOException e) {
             logger.info(e.getMessage());
         }
+    }
+
+    private static String getFileName(String chatId) {
+        return "D://" + chatId + ".json";
+//        return "/home/gnn/" + chatId + ".json";
     }
 }
